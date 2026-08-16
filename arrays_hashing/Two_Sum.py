72086fcecd245fcd62866af8d25cc596814b573e
Thomaszh <98219402+Thomaszh0000@users.noreply.github.com>
2026-08-15 22:26:44 -0400

    Enhance twoSum function with docstring and comments
    Added docstring explaining space and time complexity, and approach for the twoSum function.

---
+++ arrays_hashing/Two_Sum.py
@@ -0,0 +1,16 @@
+"""
+space complexity : O(n)
+time complexity : O(n)
+https://leetcode.com/problems/two-sum/
+Approach : Using hashtable to store complement of each element ( target - element ) along with its index. For each element,
+check whether it is already in the hashtable; if it is, it means we have found two indices we want.
+"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            if num in seen:
+                return [seen[num], i]
+            else:
+                seen[target - num] = i
+              
