293fc4c86b540b6fcd71aef4a062f0264254df66
Thomaszh <98219402+Thomaszh0000@users.noreply.github.com>
2026-08-15 23:24:57 -0400

    Improve docstring in twoSum.py
    Updated docstring to clarify approach and added problem link.

--- arrays_hashing/twoSum.py
+++ arrays_hashing/twoSum.py
@@ -1,8 +1,10 @@
 """
 space complexity : O(n)
 time complexity : O(n)
+Problems :
 https://leetcode.com/problems/two-sum/
-Approach : Using hashtable to store complement of each element ( target - element ) along with its index. For each element,
+Approach : 
+Using hashtable to store complement of each element ( target - element ) along with its index. For each element,
 check whether it is already in the hashtable; if it is, it means we have found two indices we want.
 """
 class Solution:
