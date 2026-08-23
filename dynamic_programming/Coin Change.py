766c99bea93c140a5c60c16a8718e9c2b23bbd13
Thomaszh <98219402+Thomaszh0000@users.noreply.github.com>
2026-08-22 20:18:07 -0400

    Update Coin Change.py

--- dynamic_programming/Coin Change.py
+++ dynamic_programming/Coin Change.py
@@ -9,7 +9,9 @@ class Solution:
         approach : Keep track of an DP array dp of length amount + 1, 
         which dp[i] means the minimum amount of coins needed in order to bring i up to amount.
         We use an for loop iterated from amount to 0, for each i in for loop, 
-        we will assign smallest dp[i + coin] + 1 to dp[i] (for every coin in coins and i + coin need to be smaller or equals to amount). dp[amount] will therefore be initialized to 0 (since we only need 0 when current amount is already equals to target). After iteration, we will return the answer, dp[0].
+        we will assign smallest dp[i + coin] + 1 to dp[i] (for every coin in coins and i + coin need to be smaller or equals to amount). 
+        dp[amount] will therefore be initialized to 0 (since we only need 0 when current amount is already equals to target). 
+        After iteration, we will return the answer, dp[0].
         """
         dp = [-1] * (amount + 1)
         dp[amount] = 0
