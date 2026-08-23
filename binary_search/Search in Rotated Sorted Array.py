daf57d1599f80530108084453bc2db0f718a7717
Thomaszh <98219402+Thomaszh0000@users.noreply.github.com>
2026-08-22 20:19:02 -0400

    Fix formatting in function documentation

--- binary_search/Search in Rotated Sorted Array.py
+++ binary_search/Search in Rotated Sorted Array.py
@@ -8,7 +8,8 @@ class Solution:
         approach:
         Keep track of two pointers, l (left) and r (right). While the l is less than or equal to r (using a while loop), calculate the midpoint index m. 
         If nums[m] equals to target, we have found the answer and return m.
-        If nums[m] is not equal to target, we will move r to m - 1 if target is smaller than nums[m] (search the left part), move l to m + 1 if target is bigger than nums[m] (search the right part).
+        If nums[m] is not equal to target, we will move r to m - 1 if target is smaller than nums[m] (search the left part), 
+        move l to m + 1 if target is bigger than nums[m] (search the right part).
         If l > r, it will break the while loop, this means the target isn't in the input array.
         """
         n = len(nums)
