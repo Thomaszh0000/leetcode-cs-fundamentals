5bbd82abbb2f3173e8ae97be9ab1bc16a1c36205
Thomaszh <98219402+Thomaszh0000@users.noreply.github.com>
2026-08-22 20:17:32 -0400

    Improve comment formatting in LIS algorithm
    Reformat comments for better readability.

--- dynamic_programming/Longest Increasing Subsequence.py
+++ dynamic_programming/Longest Increasing Subsequence.py
@@ -8,7 +8,9 @@ class Solution:
     Keep track of an array res. For each number in array nums, 
     if the number is bigger than res[-1], append it to the back; otherwise, 
     use binary search to find the position "pos" it should be located and change res[pos] to number. Finally, return the length of res.
-    We use bisect.bisect_left because we want to make sure that the array will always keep track of the best result (for every element, as small as possible). The length of array will be changed only if there is a input number that is bigger than res[-1], this means we have found a longer increasing subsequence.
+    We use bisect.bisect_left because we want to make sure that the array will always keep track of the best result (for every element, 
+    as small as possible). The length of array will be changed only if there is a input number that is bigger than res[-1], 
+    this means we have found a longer increasing subsequence.
     """
     def lengthOfLIS(self, nums: List[int]) -> int:
         res = [nums[0]]
