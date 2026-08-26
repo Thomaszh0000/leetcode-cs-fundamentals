e9b935b0952e54a2fcc3f9dcafc243f6cbaf4e71
Thomaszh <98219402+Thomaszh0000@users.noreply.github.com>
2026-08-25 23:13:13 -0400

    Improve comments for Reorder List algorithm
    Clarified comments in the Reorder List algorithm to improve understanding of the approach and steps involved.

--- linked_list/Reorder List.py
+++ linked_list/Reorder List.py
@@ -14,8 +14,10 @@ class Solution:
         We first keep track of two pointers fast and slow pointed at head. 
         While fast.next and fast.next.next exist, 
         we will set fast to fast.next.next and slow to slow.next, 
-        therefore slow will go to the (n//2 + 1)th (if n is odd) or (n//2)th (if n is even) ListNode, we will then set second to slow.next, prev to None and slow.next to None (we want to split the whole list into two halves).
-        Then use while-loop : while second exists, we will set ns to second.next, second.next to prev, prev to second, second to ns (so we can reverse the direction how every ListNode points at each other in the second sublist).
+        therefore slow will go to the (n//2 + 1)th (if n is odd) or (n//2)th (if n is even) ListNode, 
+        we will then set second to slow.next, prev to None and slow.next to None (we want to split the whole list into two halves).
+        Then use while-loop : while second exists, we will set ns to second.next, second.next to prev, prev to second, 
+        second to ns (so we can reverse the direction how every ListNode points at each other in the second sublist).
         We will then set first to head, second to prev.
         Use a while loop : while second exists, set n1 to first.next, n2 to second.next.
         Then we set first.next to second, second.next to n1. After that, set first to n1, second to n2.
