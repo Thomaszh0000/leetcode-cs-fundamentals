96437715239182d1bc861396ead8d68896af47b1
Thomaszh <98219402+Thomaszh0000@users.noreply.github.com>
2026-08-27 15:38:11 -0400

    Improve comments in Valid Parentheses.py
    Refactor comments for clarity in Valid Parentheses solution.

--- stack/Valid Parentheses.py
+++ stack/Valid Parentheses.py
@@ -7,7 +7,8 @@ class Solution:
         approach :
         Use a dict "convert" to store the mapping relationship between '({[' and ')}]',
         then keep track of an array stack.
-        Use a for-loop : for c in s, if c equals to any of "[" or "(" or "{", append c to the back of stack; if it is not, if stack is not empty (to prevent the situation that the number of close brackets are more than that of open brackets) and stack.pop() equals to convert[c],
+        Use a for-loop : for c in s, if c equals to any of "[" or "(" or "{", append c to the back of stack; 
+        if it is not, if stack is not empty (to prevent the situation that the number of close brackets are more than that of open brackets) and stack.pop() equals to convert[c],
         continue, otherwise, return False.
         After iteration, if stack is [], return True, else return False (means there're open brackets left).
         """
