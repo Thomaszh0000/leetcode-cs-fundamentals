3a5518873966d069394fd1de3a616e2cace45ade
Thomaszh <98219402+Thomaszh0000@users.noreply.github.com>
2026-08-28 15:37:07 -0400

    Improve docstring for DFS function in Number of Islands
    Refactor the docstring to improve clarity and readability.

--- graphs/Number of Islands.py
+++ graphs/Number of Islands.py
@@ -13,7 +13,10 @@ class Solution:
         Define a function dfs(y,x), which will check whether grid[y][x] is "0"; 
         if it is "0", the function will be exited; if it is "1", 
         the function will then change grid[y][x] from "1" to "0". 
-        After that, the function will check if y > 0, then execute dfs(y - 1, x); if y < m - 1, dfs(y + 1, x); if x > 0, dfs(y, x - 1); if x < n - 1, dfs(y, x + 1). Therefore, if we are sure there's one element in grid that is "1", we will then add one to our result and eliminate the whole island we encountered. After iterating the function through the whole grid (if grid[y][x] is "1", add 1 to result and execute dfs(y,x)), we will then return our result.
+        After that, the function will check if y > 0, then execute dfs(y - 1, x); if y < m - 1, dfs(y + 1, x); 
+        if x > 0, dfs(y, x - 1); if x < n - 1, dfs(y, x + 1). 
+        Therefore, if we are sure there's one element in grid that is "1", we will then add one to our result and eliminate the whole island we encountered. 
+        After iterating the function through the whole grid (if grid[y][x] is "1", add 1 to result and execute dfs(y,x)), we will then return our result.
         """
         n, m = len(grid[0]), len(grid)
         count = 0
