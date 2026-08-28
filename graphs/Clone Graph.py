552ae2786bc493fd5d8e54f32f01d740b07c5d25
Thomaszh <98219402+Thomaszh0000@users.noreply.github.com>
2026-08-28 15:36:44 -0400

    Improve comments in Clone Graph implementation
    Refactor comments for better readability and clarity.

--- graphs/Clone Graph.py
+++ graphs/Clone Graph.py
@@ -19,7 +19,11 @@ class Solution:
         If node is None, we will just return None.
         If node exists, we define a dict "seen" stores the mapping relationship between cloned nodes and real ones (for example, seen[real] = clone means "real" corresponds to "clone").
         We will first create a Node-class object "res" and set its value to the given node's value.
-        Then define a function dfs(real, clone) which will iterate through all neighbor in real.neighbors, if neighbor does not exist in seen, we will create a Node-class object "temp" and set its value to neighbor's value, then append it to the clone's neighbors list, and then run dfs(neighbor, temp) to continue the deep-first-search; if the neighbor already exists in seen, we will just appends seen[neighbor] to clone.neighbors (since we don't want to have two object representing the same node and this can prevent infinite loop).
+        Then define a function dfs(real, clone) which will iterate through all neighbor in real.neighbors, 
+        if neighbor does not exist in seen, we will create a Node-class object "temp" and set its value to neighbor's value, then append it to the clone's neighbors list, 
+        and then run dfs(neighbor, temp) to continue the deep-first-search; 
+        if the neighbor already exists in seen, we will just appends seen[neighbor] to clone.neighbors 
+        (since we don't want to have two object representing the same node and this can prevent infinite loop).
         Finally, execute dfs(node, res) and return the result res.
         """
         if not node:
